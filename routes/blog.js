ae913722e835d65d144254c42f33f7f1c2fdd836
Praveen <praveenrams766@gmail.com>
2025-03-28 00:21:01 +0530

    Updated dependencies and blog routes

--- routes/blog.js
+++ routes/blog.js
@@ -2,23 +2,41 @@ const {Router}= require ('express');
 const router = Router();
 const Blog= require("../models/blog");
 const Comment= require('../models/comments')
-
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
 
 const multer = require("multer");
 const path = require ('path');
 const blog = require('../models/blog');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.resolve('./public/uploads'))
-    },
-    filename: function (req, file, cb) {
-      const fileName = `${Date.now()} + ${file.originalname}`
-      cb(null,fileName);
-    }
-  });
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, path.resolve('./public/uploads'))
+//     },
+//     filename: function (req, file, cb) {
+//       const fileName = `${Date.now()} + ${file.originalname}`
+//       cb(null,fileName);
+//     }
+//   });
 
-  const upload = multer({ storage: storage })
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Configure Multer-Cloudinary Storage
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "blog_images",
+    format: async (req, file) => "png", // or 'jpeg', 'jpg'
+    public_id: (req, file) => `${Date.now()}_${file.originalname}`,
+  },
+});
+
+
+   const upload = multer({ storage: storage });
 
 router.get('/add-blog',(req,res)=>{
 return res.render('blog',{
@@ -26,14 +44,16 @@ return res.render('blog',{
 })  
 });
 
+
+
 router.post('/upload', upload.single('coverImage'), async(req,res)=>{
     const {title,body}= req.body;
      const blog =await Blog.create({
         title,
         body,
         createdBy:req.user._id,
-        CoverImgUrl:`/uploads/${req.file.filename}`
-    })
+        CoverImgUrl:req.file.path, 
+    });
       return res.redirect(`/blog/${blog._id}`);
     
 })
